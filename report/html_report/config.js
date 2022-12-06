546fd3e8546c0f78ee71ecb4fa5bba63fdb8b224
Oleksandr Kardash <sashonchic@gmail.com>
2022-12-06 16:30:24 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20221205-171802\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20221206-132424\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
